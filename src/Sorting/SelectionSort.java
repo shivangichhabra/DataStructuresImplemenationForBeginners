b7083ace5131ac6039a670ec66db797b01b1b017
Shivangi <shivangi.chhabra@cerner.com>
2017-09-05 23:17:13 -0500

    selection sort updated

--- src/Sorting/SelectionSort.java
+++ src/Sorting/SelectionSort.java
@@ -9,14 +9,14 @@ public class SelectionSort {
 
         for(int i=0; i<a.length-1; i++){
             int min = i;
-            for(int j=i+1; j<a.length; j++){
-                if(a[min] > a[j])
+            for(int j=i+1; j<a.length; j++) {
+                if (a[min] > a[j])
                     min = j;
-
-                int temp = a[i];
-                a[i] = a[min];
-                a[min] = temp;
             }
+
+            int temp = a[i];
+            a[i] = a[min];
+            a[min] = temp;
         }
 
 
